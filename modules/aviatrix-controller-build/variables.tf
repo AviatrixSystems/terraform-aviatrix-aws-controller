ca81fc7915f8fa68f4427ac0e94a10b2aab80931
Rodman <rodmans@aviatrix.com>
2023-05-01 12:31:15 -0700

    Update variables.tf

--- modules/aviatrix-controller-build/variables.tf
+++ modules/aviatrix-controller-build/variables.tf
@@ -115,7 +115,7 @@ data "aws_region" "current" {}
 data "aws_availability_zones" "all" {}
 
 data "aws_ec2_instance_type_offering" "offering" {
-  for_each = toset(data.aws_availability_zones.all.names)
+  for_each = {for zone_name in data.aws_availability_zones.all.names: zone_name => zone_name} # toset(data.aws_availability_zones.all.names)
   
   filter {
     name   = "instance-type"
